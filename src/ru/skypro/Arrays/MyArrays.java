a3c5450b56cf16975fe6d6a41e467fe1a7365386
Naletov <m.y.naletov@ya.ru>
2022-04-27 21:33:41 +0300

    Lesson_6: Strings. Исправлено!

--- src/ru/skypro/Arrays/MyArrays.java
+++ src/ru/skypro/Arrays/MyArrays.java
@@ -12,7 +12,7 @@ public class MyArrays {
     }
     public static void main(String[] args) {
         System.out.println("Массивы: ");
-//задача №1
+    //задача №1
         System.out.println("--------------------------------------------------");
         System.out.println("Задача №1 :");
         System.out.println("  ");
@@ -21,7 +21,7 @@ public class MyArrays {
             sum = sum + element;
         }
         System.out.println("Сумма трат за месяц составила "+sum+" рублей.");
-//задача №2
+    //задача №2
         System.out.println("--------------------------------------------------");
         System.out.println("Задача №2 :");
         System.out.println("  ");
@@ -36,32 +36,30 @@ public class MyArrays {
             }
 
         }
-        System.out.println();
         System.out.println("Минимальная сумма трат за день составила "+maxCoin+" рублей.");
         System.out.println("Максимальная сумма трат за день составила "+minCoin+" рублей.");
-//задача №3
+    //задача №3
         System.out.println("--------------------------------------------------");
         System.out.println("Задача №3 :");
         System.out.println("  ");
         int daysMonth = 30;
         var mean = sum / daysMonth;
         System.out.println("Средняя сумма трат за месяц составила "+mean+" рублей.");
-//задача №4
+    //задача №4
         System.out.println("--------------------------------------------------");
         System.out.println("Задача №4 :");
         System.out.println("  ");
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        System.out.printf(Arrays.toString(reverseFullName));
         int size = reverseFullName.length;
         for (int i = 0; i < size / 2; i++) {
             char temp = reverseFullName[i];
             reverseFullName[i] = reverseFullName[size - 1 - i];
             reverseFullName[size - 1 - i] = temp;
         }
+        for (char c : reverseFullName) {
+            System.out.printf(String.valueOf(c));
+        }
         System.out.println(" ");
-        System.out.printf(Arrays.toString(reverseFullName));
-        System.out.println(" ");
-        System.out.println("Баг программы исправлен! ");
         System.out.println("--------------------------------------------------");
         System.out.println("End.");
         System.out.println("  ");
