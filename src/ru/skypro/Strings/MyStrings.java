fd2a6cd50fd2b6c2eeae8ccc9353dd6713dec113
Naletov <m.y.naletov@ya.ru>
2022-04-27 22:12:42 +0300

    Lesson_6: Strings. ошибки_исправлены!

--- src/ru/skypro/Strings/MyStrings.java
+++ src/ru/skypro/Strings/MyStrings.java
@@ -10,35 +10,27 @@ public class MyStrings {
         System.out.println("Строки:");
         System.out.println();
 
-//задача №1,2,3,4:
+    //задача №1:
         System.out.println("Задача №1 --------------------------------------------------");
-        String firstName = "Ivan ";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleName;
+        String firstName = "Ivan";
+        String middleName ="Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName +" "+ firstName+" "+ middleName;
         System.out.println("ФиО сотрудника - " + fullName);
-
+    //задача №2:
         System.out.println("Задача №2 --------------------------------------------------");
-        System.out.println("Данные ФиО сотрудника для заполнения отчета — "+fullName.toUpperCase());
-
-//        System.out.println();
-//        String q = fullName;
-//        String q1 = q.toUpperCase();
-//        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+q1);
-
+        String q;
+        q = fullName;
+        String q1 = q.toUpperCase();
+        System.out.println("Данные ФиО сотрудника для заполнения отчета — "+q1);
+    //задача №3:
         System.out.println("Задача №3 --------------------------------------------------");
-        System.out.println("Данные ФиО сотрудника для административного отдела — " + fullName.replace(' ', ';'));
-
-//        System.out.println();
-//        String q2 = q.replace(' ',';');
-//        System.out.println("Данные ФИО сотрудника для административного отдела — "+q2);
-
+        String q2 = q.replace(" ","; ");
+        System.out.println("Данные ФиО сотрудника для административного отдела — "+q2);
+    //задача №4:
         System.out.println("Задача №4 --------------------------------------------------");
         String rusFullName = "Иванов Семён Семёнович";
         System.out.println("Данные ФиО сотрудника — " +rusFullName.replace("ё", "е"));
-
-//        rusFullName = rusFullName.replace("ё", "е");
-//        System.out.println("Данные ФиО сотрудника — " + rusFullName);
         System.out.println("------------------------------------------------------------");
         System.out.println("End.");
     }
